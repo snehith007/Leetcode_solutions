bc61fb3d8098f46bf3887d583f7c3ec0789a45e0
Snehith007 <msnehith007@gmail.com>
2024-12-17 09:18:52 +0530

    ..

---
+++ 42.Trapping_Rain_Water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l =0;
+        int r=height.size() -1;
+        int leftmax = 0;
+        int rightmax=0;
+        int ans =0;
+
+        while(l<=r){
+
+            if(height[l] <= height[r]){
+
+                if(height[l] >= leftmax) leftmax = height[l];
+                else ans+= leftmax - height[l];
+
+                l++;
+            }
+            else{
+
+                if(height[r] >=rightmax) rightmax = height[r];
+                else ans += rightmax-height[r];
+
+                r--;
+            }
+        }
+        return ans;
+        
+    }
+};
