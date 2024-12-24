7b72eaa770bdaebf5813cc9ccabf140df060eb38
Snehith007 <msnehith007@gmail.com>
2024-12-24 13:08:31 +0530

    ..

---
+++ 58.Length_of_Last_Word.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int ans = 0;
+        for(int i= s.length() - 1; i>=0;i--){
+            if(s[i] != ' '){
+                ans++;
+            }
+            else{
+                if(ans > 0){
+                    return ans;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
